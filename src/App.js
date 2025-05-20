bd707b91119ca8989e9f1530bd72e78d27ee1220
Xrpdgard <workout.kievregion@gmail.com>
2025-05-20 21:57:54 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main className='min-h-screen bg-white text-center p-10'>
+      <h1 className='text-4xl font-bold text-blue-700 mb-4'>United for Sport</h1>
+      <p className='text-lg text-gray-700'>Це перевірена версія сайту. Все працює!</p>
+    </main>
+  );
+}
